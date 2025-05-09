02516a9c0eea5ddb8b20da488650e4d195f7806a
SchertzS <SchertzS49@gmail.com>
2025-05-08 17:54:05 -0700

    feat: add Angular Material tooltip and progress bar modules to skill meter component

--- src/app/components/team-formation/skill-meter/skill-meter.component.ts
+++ src/app/components/team-formation/skill-meter/skill-meter.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-skill-meter',
-  imports: [],
+  imports: [MatProgressBarModule,
+            MatTooltipModule],
   templateUrl: './skill-meter.component.html',
   styleUrl: './skill-meter.component.scss'
 })
