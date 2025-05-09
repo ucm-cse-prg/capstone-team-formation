53fbbf77049dd3c00d416e873550a947c8738f93
SchertzS <SchertzS49@gmail.com>
2025-05-08 17:33:33 -0700

    feat: add student card component with template, styles, and tests

---
+++ src/app/components/team-formation/student-card/student-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-card',
+  imports: [],
+  templateUrl: './student-card.component.html',
+  styleUrl: './student-card.component.scss'
+})
+export class StudentCardComponent {
+
+}
