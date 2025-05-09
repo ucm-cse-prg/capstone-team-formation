6975ca9d07b3e9553e7707d93d5f4507fdeb4dbc
SchertzS <SchertzS49@gmail.com>
2025-05-08 17:41:40 -0700

    feat: add Angular Material modules to team config form component

--- src/app/components/team-formation/team-config-form/team-config-form.component.ts
+++ src/app/components/team-formation/team-config-form/team-config-form.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @Component({
   selector: 'app-team-config-form',
-  imports: [],
+  imports: [MatFormFieldModule,
+            MatInputModule,
+            MatButtonModule,
+            MatSelectModule,],
   templateUrl: './team-config-form.component.html',
   styleUrl: './team-config-form.component.scss'
 })
