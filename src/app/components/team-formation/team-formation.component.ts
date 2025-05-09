9d2e2d85594142fcec38c31e51d64c388c529db4
SchertzS <SchertzS49@gmail.com>
2025-05-08 18:49:39 -0700

    feat: integrate TeamConfigFormComponent into TeamFormationComponent and add config handling logic

--- src/app/components/team-formation/team-formation.component.ts
+++ src/app/components/team-formation/team-formation.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {TeamConfigFormComponent} from '@components/team-formation/team-config-form/team-config-form.component';
 
 @Component({
   selector: 'app-team-formation',
-  imports: [],
+  imports: [TeamConfigFormComponent],
   templateUrl: './team-formation.component.html',
   styleUrl: './team-formation.component.scss'
 })
 export class TeamFormationComponent {
+  // Takes one argument (the form data from the child component and emits an event)
+  // logs data and will later be used to trigger team generation
+  handleConfig(config: { teamSize: number; preferenceWeight?: number; labLockProject?: number }) {
+    console.log('Form submitted:', config);
+    // TODO: generate teams, update service state, etc.
+  }
 
 }
