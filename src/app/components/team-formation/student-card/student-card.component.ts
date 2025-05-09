9ec9a2b2996d4846577348738e2a222663ef03cf
SchertzS <SchertzS49@gmail.com>
2025-05-08 17:49:56 -0700

    feat: add Angular Material and CDK modules to student card component

--- src/app/components/team-formation/student-card/student-card.component.ts
+++ src/app/components/team-formation/student-card/student-card.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-student-card',
-  imports: [],
+  imports: [MatCardModule,
+            MatIconModule,
+            MatTooltipModule,
+            DragDropModule],
   templateUrl: './student-card.component.html',
   styleUrl: './student-card.component.scss'
 })
