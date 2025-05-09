5ce5dcdee4314ef170943eabbfa5408ae91c4b21
SchertzS <SchertzS49@gmail.com>
2025-05-08 17:24:48 -0700

    feat: add team list component with template, styles, and tests

---
+++ src/app/components/team-formation/team-list/team-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-list',
+  imports: [],
+  templateUrl: './team-list.component.html',
+  styleUrl: './team-list.component.scss'
+})
+export class TeamListComponent {
+
+}
