eb9f922117a0f333253a3ab8b23abc3a3700f7b5
SchertzS <SchertzS49@gmail.com>
2025-05-08 19:16:10 -0700

    feat: add Input property for teams list in TeamListComponent

--- src/app/components/team-formation/team-list/team-list.component.ts
+++ src/app/components/team-formation/team-list/team-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TeamCardComponent} from '@components/team-formation/team-card/team-card.component';
+import {Team} from '@models/team.model';
 
 @Component({
   selector: 'app-team-list',
@@ -9,6 +10,8 @@ import {TeamCardComponent} from '@components/team-formation/team-card/team-card.
   templateUrl: './team-list.component.html',
   styleUrl: './team-list.component.scss'
 })
+// The TeamListComponent responsible for displaying a list of teams.
 export class TeamListComponent {
-
+  // Input property to receive the list of teams from the parent component
+  @Input() teams: Team[] = [];
 }
