d765a9aa311c660dfb5403809621609a997c71b1
SchertzS <SchertzS49@gmail.com>
2025-05-08 17:47:46 -0700

    feat: add Angular Material modules to team card component

--- src/app/components/team-formation/team-card/team-card.component.ts
+++ src/app/components/team-formation/team-card/team-card.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-team-card',
-  imports: [],
+  imports: [MatCardModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatProgressBarModule],
   templateUrl: './team-card.component.html',
   styleUrl: './team-card.component.scss'
 })
