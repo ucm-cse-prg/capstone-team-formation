1091d4569559c41d2a33162083a4aae96491d404
SchertzS <SchertzS49@gmail.com>
2025-05-08 17:25:26 -0700

    feat: add team card component with template, styles, and tests

---
+++ src/app/components/team-formation/team-card/team-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-card',
+  imports: [],
+  templateUrl: './team-card.component.html',
+  styleUrl: './team-card.component.scss'
+})
+export class TeamCardComponent {
+
+}
