807a1c355af33a34bf34e589869fee4b9ea9f40d
SchertzS <SchertzS49@gmail.com>
2025-05-08 19:14:47 -0700

    feat: add CommonModule and TeamCardComponent imports to team list component

--- src/app/components/team-formation/team-list/team-list.component.ts
+++ src/app/components/team-formation/team-list/team-list.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TeamCardComponent} from '@components/team-formation/team-card/team-card.component';
 
 @Component({
   selector: 'app-team-list',
-  imports: [],
+  imports: [CommonModule,
+            TeamCardComponent],
   templateUrl: './team-list.component.html',
   styleUrl: './team-list.component.scss'
 })
